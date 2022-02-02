485ebe8754de9e6ec831bfcd52ce27e24bc4b1ff
Yavor Radulov <yavor.radulov@mentormate.com>
2022-02-02 16:28:50 +0200

    new solutions

--- 07 - Unit Testing and Error Handling/Exercise/01_request_validator.js
+++ 07 - Unit Testing and Error Handling/Exercise/01_request_validator.js
@@ -1,27 +1,24 @@
 function solve(input) {
   const valid = {
     method: ['GET', 'POST', 'DELETE', 'CONNECT'],
-    uri: /^[A-Za-z.\d]{1,}$|\*/,
+    uri: /^[\w.]+$/,
     version: ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'],
-    message: /[^\<\>\\\&\'\"]/,
+    message: /^[^<>\\&'"]*$/,
   };
 
-  if (!valid.method.includes(input.method)) {
+  if (!input.method || !valid.method.includes(input.method)) {
     throw new Error('Invalid request header: Invalid Method');
   }
 
-  if (!valid.uri.test(input.uri) || !input.uri) {
+  if (!input.uri || !valid.uri.test(input.uri)) {
     throw new Error('Invalid request header: Invalid URI');
   }
 
-  if (!valid.version.includes(input.version)) {
+  if (!input.version || !valid.version.includes(input.version)) {
     throw new Error('Invalid request header: Invalid Version');
   }
 
-  if (
-    input.message !== '' &&
-    (!valid.message.test(input.message) || !input.message)
-  ) {
+  if (!input.hasOwnProperty('message') || !valid.message.test(input.message)) {
     throw new Error('Invalid request header: Invalid Message');
   }
 
--- 07 - Unit Testing and Error Handling/Exercise/05.Notification/app.js
+++ 07 - Unit Testing and Error Handling/Exercise/05.Notification/app.js
@@ -1,4 +1,11 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+  const notification = document.getElementById('notification');
+
+  notification.innerText = message;
+  notification.style.display = 'block';
+
+  notification.addEventListener(
+    'click',
+    event => (event.target.style.display = 'none')
+  );
 }
